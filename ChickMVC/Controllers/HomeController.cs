6eedbd1d7a87b4a15cb4e0790faa8b0e7d217afc
Ryno <st10439009@vcconnect.edu.za>
2026-05-21 10:22:23 +0200

    commit

--- ChickMVC/Controllers/HomeController.cs
+++ ChickMVC/Controllers/HomeController.cs
@@ -6,8 +6,25 @@ namespace ChickMVC.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly HttpClient _httpClient;
+
+        public HomeController(HttpClient httpClient)
         {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                var response = await _httpClient.GetStringAsync("http://api-service:8080/api/chicken");
+                ViewBag.Chicken = response;
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Chicken = "Error: " + ex.Message;
+            }
+
             return View();
         }
 
